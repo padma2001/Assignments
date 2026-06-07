869fc8fa31430e9251d307b4902975cc44ac7636
K Padmavathi <kodurupadmavathi21@gmail.com>
2026-06-07 18:31:12 +0530

    assignment1.ts

--- typescript-training/assignment1.ts
+++ typescript-training/assignment1.ts
@@ -1,3 +1,29 @@
 let cityTemperature: number = 45;
 console.log("City Temperature:", cityTemperature);
 
+let order:boolean =true;
+console.log("order", order);
+
+let personphonenumber:number =1234567;
+console.log("personphonenumber",personphonenumber);
+
+let amount:number=1000.50;
+console.log("amount in the customer bank account",amount)
+
+let personalemailaddress:string="Bharat@gmail.com"
+console.log("emailaddress of the person ",personalemailaddress)
+
+let coordinates:number[]=[37.77989,-12.99786];
+console.log("coordinates:" +  coordinates[0] + ", " + coordinates[1]);
+
+let isMarried:boolean=true;
+console.log("is Married", isMarried)
+
+let ocupation:string="software engineer"
+console.log("Occupation of the person", ocupation)
+
+let currentyear:number=2026;
+console.log("current year is ",currentyear)
+
+let distnacebetweentwocities:number=200.50;
+console.log("The distance between two cities is ", distnacebetweentwocities)
